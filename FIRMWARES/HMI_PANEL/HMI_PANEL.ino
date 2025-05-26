acc9b4eb53c8375bda80319b7d688c24d318e8c6
iCruzDaniel <sp_dicruz044@hotmail.com>
2025-05-26 00:35:33 -0500

    correción de asignación de filas en teclado matricial

--- FIRMWARES/HMI_PANEL/HMI_PANEL.ino
+++ FIRMWARES/HMI_PANEL/HMI_PANEL.ino
@@ -8,7 +8,7 @@
 // --- Object Init ----
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // LCD 20x4
 
-File file; //Archivo de para guardar info
+File file;  //Archivo de para guardar info
 
 // --- Configuración del teclado ---
 const byte ROWS = 5;
@@ -20,8 +20,10 @@ char keys[ROWS][COLS] = {
   { '7', '8', '9', 'E' },
   { 'L', '0', 'R', 'N' }
 };
-byte rowPins[ROWS] = { 9, 8, 7, 6, 5 };
-byte colPins[COLS] = { 4, 3, 2, A0 };
+// byte rowPins[ROWS] = { 9, 8, 7, 6, 5 };
+byte rowPins[ROWS] = { 2, 3, 4, 5, 6 };
+// byte colPins[COLS] = { 4, 3, 2, A0 };
+byte colPins[COLS] = { A0, A1, A2, A3 };
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // --- Estados del sistema ---
@@ -59,20 +61,20 @@ const int chipSelect = 10;
 
 // ===== Contantes =====
 const int TEMP_ADDR = 0;
-const int ITER_ADDR = sizeof(byte); // sizeof(int) a sizeof(byte)
+const int ITER_ADDR = sizeof(byte);  // sizeof(int) a sizeof(byte)
 // ---------------------------------
 // ==============================
 
 
 // ===== Variables Globales =====
-byte temperatura = 20; 
-byte iteraciones = 1;  
-byte currentIteration = 1; 
+byte temperatura = 20;
+byte iteraciones = 1;
+byte currentIteration = 1;
 byte selectedOption = 0;
 // ---------------------------------
-#define MAX_INPUT_BUFFER_LEN 4 // Max 3 dígitos para temp/iter + null terminator
+#define MAX_INPUT_BUFFER_LEN 4  // Max 3 dígitos para temp/iter + null terminator
 char inputBuffer[MAX_INPUT_BUFFER_LEN];
-byte inputBufferLen = 0; // Para llevar la cuenta de los caracteres actuales
+byte inputBufferLen = 0;  // Para llevar la cuenta de los caracteres actuales
 bool sphereInPosition = false;
 // ==============================
 
@@ -88,7 +90,7 @@ bool sphereInPosition = false;
 int freeRam() {
   extern int __heap_start, *__brkval;
   int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
 }
 // ---------------------------------
 
@@ -99,12 +101,19 @@ int freeRam() {
 
 // ===== MAIN =====
 void setup() {
+  //=================
+  pinMode(7, OUTPUT);
+  digitalWrite(7, 1);
+  delay(4000);
+  digitalWrite(7, 0);
+  //=================
+
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
 
   // Inicializar inputBuffer
-  inputBuffer[0] = '\0'; // Asegurar que esté vacío al inicio
+  inputBuffer[0] = '\0';  // Asegurar que esté vacío al inicio
   inputBufferLen = 0;
 
   EEPROM.get(TEMP_ADDR, temperatura);
@@ -113,17 +122,18 @@ void setup() {
   if (temperatura < 0 || temperatura > 100) temperatura = 20;
   if (iteraciones < 1) iteraciones = 1;
 
-  Serial.println(F("Inicializando tarjeta SD...")); 
-  if (!SD.begin(chipSelect)) {
-    Serial.println(F("Error: Fallo al inicializar la SD.")); 
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(F("Fallo SD!")); 
-    while (true); // Detiene el programa aquí para que veas el error
-  }
-  Serial.println(F("SD inicializada correctamente.")); 
-  Serial.print(F("RAM libre al inicio: ")); 
-  Serial.println(freeRam());
+  // Serial.println(F("Inicializando tarjeta SD..."));
+  // if (!SD.begin(chipSelect)) {
+  //   Serial.println(F("Error: Fallo al inicializar la SD."));
+  //   lcd.clear();
+  //   lcd.setCursor(0, 0);
+  //   lcd.print(F("Fallo SD!"));
+  //   while (true)
+  //     ;  // Detiene el programa aquí para que veas el error
+  // }
+  // Serial.println(F("SD inicializada correctamente."));
+  // Serial.print(F("RAM libre al inicio: "));
+  // Serial.println(freeRam());
 
 
   showStartScreen();
@@ -133,7 +143,7 @@ void setup() {
 void loop() {
   char key = keypad.getKey();
   if (key) handleKey(key);
-  
+
   // Leer la trama completa en el Arduino
   if (Serial.available() > 0) {
     delay(10);
@@ -195,13 +205,13 @@ void loop() {
 void showStartScreen() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F(">> Inicializando <<")); 
+  lcd.print(F(">> Inicializando <<"));
   lcd.setCursor(3, 1);
-  lcd.print(F("--Ensayo de")); 
+  lcd.print(F("--Ensayo de"));
   lcd.setCursor(3, 2);
-  lcd.print(F("Viscosidad --")); 
+  lcd.print(F("Viscosidad --"));
   lcd.setCursor(5, 3);
-  lcd.print(F("Espere...")); 
+  lcd.print(F("Espere..."));
   delay(2500);
   currentState = MAIN_MENU;
   showMainMenu();
@@ -210,95 +220,95 @@ void showStartScreen() {
 void showMainMenu() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("--Menu Principal--")); 
+  lcd.print(F("--Menu Principal--"));
 
   lcd.setCursor(1, 2);
-  lcd.print(F("Nuevo ensayo")); 
-  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<")); 
+  lcd.print(F("Nuevo ensayo"));
+  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<"));
 
   lcd.setCursor(1, 3);
-  lcd.print(F("Config. de HMI")); 
-  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<")); 
+  lcd.print(F("Config. de HMI"));
+  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<"));
 }
 
 void showNewTestMenu() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("--Inicio y Config--")); 
+  lcd.print(F("--Inicio y Config--"));
 
   lcd.setCursor(1, 2);
-  lcd.print(F("Iniciar ensayo")); 
-  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<")); 
+  lcd.print(F("Iniciar ensayo"));
+  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<"));
 
   lcd.setCursor(1, 3);
-  lcd.print(F("Config. Variables")); 
-  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<")); 
+  lcd.print(F("Config. Variables"));
+  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<"));
 }
 
 void showConfigVarsMenu() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("-Config. Variables-")); 
+  lcd.print(F("-Config. Variables-"));
 
   lcd.setCursor(1, 1);
-  lcd.print(F("Temperatura = ")); 
+  lcd.print(F("Temperatura = "));
   lcd.print(temperatura);
-  if (selectedOption == 0) lcd.setCursor(19, 1), lcd.print(F("<")); 
+  if (selectedOption == 0) lcd.setCursor(19, 1), lcd.print(F("<"));
 
   lcd.setCursor(1, 2);
-  lcd.print(F("Iteraciones = ")); 
+  lcd.print(F("Iteraciones = "));
   lcd.print(iteraciones);
-  if (selectedOption == 1) lcd.setCursor(19, 2), lcd.print(F("<")); 
+  if (selectedOption == 1) lcd.setCursor(19, 2), lcd.print(F("<"));
 
   lcd.setCursor(1, 3);
-  lcd.print(F("Ent (Modificar)")); 
+  lcd.print(F("Ent (Modificar)"));
 }
 
 void showEditTemp() {
   lcd.clear();
   lcd.setCursor(1, 0);
-  lcd.print(F("Nueva temperatura:")); 
+  lcd.print(F("Nueva temperatura:"));
   lcd.setCursor(1, 2);
-  lcd.print(F("> ")); 
-  lcd.print(inputBuffer); // Imprimir el char array
+  lcd.print(F("> "));
+  lcd.print(inputBuffer);  // Imprimir el char array
   lcd.setCursor(1, 3);
-  lcd.print(F("Rango: 0-100")); 
+  lcd.print(F("Rango: 0-100"));
 }
 
 void showEditIter() {
   lcd.clear();
   lcd.setCursor(1, 0);
-  lcd.print(F("Nuevas iteraciones:")); 
+  lcd.print(F("Nuevas iteraciones:"));
   lcd.setCursor(1, 2);
-  lcd.print(F("> ")); 
-  lcd.print(inputBuffer); // Imprimir el char array
+  lcd.print(F("> "));
+  lcd.print(inputBuffer);  // Imprimir el char array
   lcd.setCursor(1, 3);
-  lcd.print(F("Minimo: 1")); 
+  lcd.print(F("Minimo: 1"));
 }
 
 void showTestRunningScreen() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("-Ejecutando Ensayo-")); 
+  lcd.print(F("-Ejecutando Ensayo-"));
 
   lcd.setCursor(1, 1);
-  lcd.print(F("Iter. = ")); 
+  lcd.print(F("Iter. = "));
   lcd.print(currentIteration);
 
   lcd.setCursor(1, 2);
-  lcd.print(F("Esfera en posicion?")); 
+  lcd.print(F("Esfera en posicion?"));
 
   lcd.setCursor(1, 3);
-  lcd.print(F("Si")); 
-  if (selectedOption == 0) lcd.setCursor(18, 3), lcd.print(F("<")); 
+  lcd.print(F("Si"));
+  if (selectedOption == 0) lcd.setCursor(18, 3), lcd.print(F("<"));
 }
 
 void showSphereReleased() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("-Ensayo en Progreso-")); 
+  lcd.print(F("-Ensayo en Progreso-"));
   lcd.setCursor(2, 2);
-  lcd.print(F("Esfera Liberada!")); 
+  lcd.print(F("Esfera Liberada!"));
   delay(2000);
 
   if (currentIteration < iteraciones) {
@@ -316,47 +326,47 @@ void showSphereReleased() {
 void showSaveDataQuestion() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(F("-Guardar resultados?-")); 
+  lcd.print(F("-Guardar resultados?-"));
 
   lcd.setCursor(1, 2);
-  lcd.print(F("Si")); 
-  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<")); 
+  lcd.print(F("Si"));
+  if (selectedOption == 0) lcd.setCursor(18, 2), lcd.print(F("<"));
 
   lcd.setCursor(1, 3);
-  lcd.print(F("No")); 
-  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<")); 
+  lcd.print(F("No"));
+  if (selectedOption == 1) lcd.setCursor(18, 3), lcd.print(F("<"));
 }
 
 void saveDataToSD() {
-  Serial.print(F("RAM libre antes de abrir archivo SD: ")); 
+  Serial.print(F("RAM libre antes de abrir archivo SD: "));
   Serial.println(freeRam());
 
   file = SD.open("data.txt", FILE_WRITE);
 
   if (file) {
-    Serial.print(F("RAM libre despues de abrir y antes de escribir SD: ")); 
+    Serial.print(F("RAM libre despues de abrir y antes de escribir SD: "));
     Serial.println(freeRam());
 
-    file.print(F("Temperatura: ")); 
+    file.print(F("Temperatura: "));
     file.println(temperatura);
-    file.print(F("Iteraciones: ")); 
+    file.print(F("Iteraciones: "));
     file.println(iteraciones);
-    file.println(F("------")); 
+    file.println(F("------"));
     file.close();
 
-    Serial.print(F("RAM libre despues de cerrar archivo SD: ")); 
+    Serial.print(F("RAM libre despues de cerrar archivo SD: "));
     Serial.println(freeRam());
 
     lcd.clear();
     lcd.setCursor(2, 1);
-    lcd.print(F("Datos guardados")); 
+    lcd.print(F("Datos guardados"));
     delay(2000);
   } else {
     lcd.clear();
     lcd.setCursor(2, 1);
-    lcd.print(F("Error al guardar")); 
+    lcd.print(F("Error al guardar"));
     delay(2000);
-    Serial.print(F("RAM libre despues de error de guardar SD: ")); 
+    Serial.print(F("RAM libre despues de error de guardar SD: "));
     Serial.println(freeRam());
   }
 
@@ -365,7 +375,7 @@ void saveDataToSD() {
 }
 
 void handleKey(char key) {
-  
+
   switch (currentState) {
     case MAIN_MENU:
       if (key == 'U' || key == 'D') selectedOption = (selectedOption + 1) % 2, showMainMenu();
@@ -387,12 +397,13 @@ void handleKey(char key) {
       else if (key == 'N') {
         if (selectedOption == 0) {
           currentState = EDIT_TEMP;
-          inputBufferLen = 0; inputBuffer[0] = '\0'; // Limpiar buffer
+          inputBufferLen = 0;
+          inputBuffer[0] = '\0';  // Limpiar buffer
           showEditTemp();
-        }
-        else if (selectedOption == 1) {
+        } else if (selectedOption == 1) {
           currentState = EDIT_ITER;
-          inputBufferLen = 0; inputBuffer[0] = '\0'; // Limpiar buffer
+          inputBufferLen = 0;
+          inputBuffer[0] = '\0';  // Limpiar buffer
           showEditIter();
         }
       } else if (key == 'E') currentState = NEW_TEST_MENU, showNewTestMenu();
@@ -401,25 +412,24 @@ void handleKey(char key) {
     case EDIT_TEMP:
       if ((key >= '0' && key <= '9') && inputBufferLen < (MAX_INPUT_BUFFER_LEN - 1)) {
         inputBuffer[inputBufferLen++] = key;
-        inputBuffer[inputBufferLen] = '\0'; // Asegurar el terminador nulo
+        inputBuffer[inputBufferLen] = '\0';  // Asegurar el terminador nulo
         showEditTemp();
-      }
-      else if (key == 'L' && inputBufferLen > 0) {
-        inputBuffer[--inputBufferLen] = '\0'; // Retroceder y poner terminador nulo
+      } else if (key == 'L' && inputBufferLen > 0) {
+        inputBuffer[--inputBufferLen] = '\0';  // Retroceder y poner terminador nulo
         showEditTemp();
-      }
-      else if (key == 'E') currentState = CONFIG_VARS_MENU, showConfigVarsMenu();
+      } else if (key == 'E') currentState = CONFIG_VARS_MENU, showConfigVarsMenu();
       else if (key == 'N' && inputBufferLen > 0) {
-        int newTemp = atoi(inputBuffer); // Usar atoi() para convertir char[] a int
+        int newTemp = atoi(inputBuffer);  // Usar atoi() para convertir char[] a int
         if (newTemp >= 0 && newTemp <= 100) {
-          temperatura = (byte)newTemp; // Cast a byte
+          temperatura = (byte)newTemp;  // Cast a byte
           EEPROM.put(TEMP_ADDR, temperatura);
           currentState = CONFIG_VARS_MENU;
           showConfigVarsMenu();
         } else {
-          lcd.setCursor(1, 3), lcd.print(F("Error: Fuera rango!")); 
+          lcd.setCursor(1, 3), lcd.print(F("Error: Fuera rango!"));
           delay(1500);
-          inputBufferLen = 0; inputBuffer[0] = '\0'; // Resetear buffer
+          inputBufferLen = 0;
+          inputBuffer[0] = '\0';  // Resetear buffer
           showEditTemp();
         }
       }
@@ -432,23 +442,22 @@ void handleKey(char key) {
         inputBuffer[inputBufferLen++] = key;
         inputBuffer[inputBufferLen] = '\0';
         showEditIter();
-      }
-      else if (key == 'L' && inputBufferLen > 0) {
+      } else if (key == 'L' && inputBufferLen > 0) {
         inputBuffer[--inputBufferLen] = '\0';
         showEditIter();
-      }
-      else if (key == 'E') currentState = CONFIG_VARS_MENU, showConfigVarsMenu();
+      } else if (key == 'E') currentState = CONFIG_VARS_MENU, showConfigVarsMenu();
       else if (key == 'N' && inputBufferLen > 0) {
-        int newIter = atoi(inputBuffer); // Usar atoi()
+        int newIter = atoi(inputBuffer);  // Usar atoi()
         if (newIter >= 1) {
-          iteraciones = (byte)newIter; // Cast a byte
+          iteraciones = (byte)newIter;  // Cast a byte
           EEPROM.put(ITER_ADDR, iteraciones);
           currentState = CONFIG_VARS_MENU;
           showConfigVarsMenu();
         } else {
-          lcd.setCursor(1, 3), lcd.print(F("Error: Minimo 1!")); 
+          lcd.setCursor(1, 3), lcd.print(F("Error: Minimo 1!"));
           delay(1500);
-          inputBufferLen = 0; inputBuffer[0] = '\0'; // Resetear buffer
+          inputBufferLen = 0;
+          inputBuffer[0] = '\0';  // Resetear buffer
           showEditIter();
         }
       }
@@ -457,7 +466,8 @@ void handleKey(char key) {
     case SPHERE_POSITION_QUESTION:
       if (key == 'N' && selectedOption == 0) {
         sphereInPosition = true;
-        Serial.print(F("SphereInPosition: ")); Serial.println(sphereInPosition); 
+        Serial.print(F("SphereInPosition: "));
+        Serial.println(sphereInPosition);
         currentState = SPHERE_RELEASED;
         showSphereReleased();
       } else if (key == 'E') currentState = NEW_TEST_MENU, showNewTestMenu();
